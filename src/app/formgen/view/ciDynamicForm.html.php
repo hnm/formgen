e5ce6c48383de94c6d3f80e9db3f1051c2868fea
Thomas Guenther <guenther@hnm.ch>
2026-03-16 15:59:10 +0100

    updates

--- src/app/formgen/view/ciDynamicForm.html.php
+++ src/app/formgen/view/ciDynamicForm.html.php
@@ -1,15 +1,22 @@
 <?php
 	use n2n\impl\web\ui\view\html\HtmlView;
 	use formgen\bo\DynamicForm;
-use formgen\model\FormgenDynamicForm;
-	
+	use formgen\model\FormgenDynamicForm;
+	use formgen\model\FormgenConfig;
+		
 	$view = HtmlView::view($view);
 	$html = HtmlView::html($view);
 	
 	$dynamicForm = $view->getParam('dynamicForm');
 	$view->assert($dynamicForm instanceof DynamicForm);
+	
+	$formgenConfig = $view->lookup(FormgenConfig::class);
+	$view->assert($formgenConfig instanceof FormgenConfig);
+	
+	$html->meta()->addJs('formgen.js');
 ?>
-<div class="formgen-ci-dynamic-form">
+<div class="formgen-ci-dynamic-form" 
+		data-reload-url="<?php $html->out($view->buildUrlStr($formgenConfig->buildGetDynmicFormPartUrl($dynamicForm))) ?>">
 	<?php $view->import('inc\dynamicFormPart.html', 
 			array('formgenDynamicForm' => new FormgenDynamicForm($view->getN2nLocale(), $dynamicForm))) ?>
 </div>
